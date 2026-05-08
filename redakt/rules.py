27fbb4e53edb5faf6215704cd0e4282121a56736
architjambhule66-debug <architjambhule66@gmail.com>
2026-05-08 19:46:11 +0530

    add redaction modes and CLI rule management

--- redakt/rules.py
+++ redakt/rules.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass, field
 from typing import Dict, List, Optional, Tuple, Callable
 from enum import Enum 
 
+
 class DetectionMethod(str, Enum):
     REGEX = "regex"
     NER = "ner"
